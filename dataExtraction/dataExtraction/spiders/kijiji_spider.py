392b31bee04b51bf11b3fae6fd359635beeea56c
Uri Trejo <utrej018@uottawa.ca>
2020-02-28 11:55:31 -0500

    Added global list storing ads, checking for new ads

--- dataExtraction/dataExtraction/spiders/kijiji_spider.py
+++ dataExtraction/dataExtraction/spiders/kijiji_spider.py
@@ -2,9 +2,12 @@ import scrapy
 
 class KijijiSpider(scrapy.Spider):
 
+    cars = [] #later a hash map could optimize search
+
     name = "kijiji"
     start_urls = {
-        'https://www.kijiji.ca/b-ontario/honda-civic/k0l9004?dc=true'
+        # 'https://www.kijiji.ca/b-ontario/honda-civic/k0l9004?dc=true' #Ontario
+        'https://www.kijiji.ca/b-cars-trucks/ottawa-gatineau-area/honda-civic/k0c174l1700184' #Ottawa
     }
 
     def parse(self, response):
@@ -16,9 +19,19 @@ class KijijiSpider(scrapy.Spider):
         print("Starting round of collection.")
 
         for car in car_ad_titles:
-            print(car)
 
-        # print(car_ad_titles[2])
+            if car not in self.cars:
+
+                print("New Car:")
+                print(car)
+                self.cars.append(car)
+
+        # problem: the cars array and the prices array are of different lengths
+        # so idk how I'm gonna map the prices to the car
+        # Maybe if i use the blocks like the guy in the video it makes it easier, there has to be a way
+        print("Size of cars array: ", len(self.cars))
+        # print("Size of cars array: ", len(car_ad_titles))
+        # print("Size of prices array: ", len(prices))
 
 
         yield {
