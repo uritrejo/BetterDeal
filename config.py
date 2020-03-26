67e245375c3f0b6e39ec86b55d4c15e3598dc4a7
Uri Trejo <utrej018@uottawa.ca>
2020-03-26 14:56:18 -0400

    modified config file to run less traffic in the network

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ CAR_MODELS = {
 }
 
 # time in seconds between rounds of data collection
-TIME_BETWEEN_ROUNDS = 10
+TIME_BETWEEN_ROUNDS = 240
 
 # number of rounds to ignore (no email notifications for new cars will be sent for the first ROUNDS_TO_IGNORE rounds)
 # this is to avoid overload of notifications at the beginning, when everything is new
-ROUNDS_TO_IGNORE = 20
+ROUNDS_TO_IGNORE = 7
